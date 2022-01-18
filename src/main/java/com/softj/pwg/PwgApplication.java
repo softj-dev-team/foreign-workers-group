03ab2ee5cdc28d8fa2c2495013221a4365bda533
RoyDaddy <tirran@naver.com>
2022-01-18 14:19:00 +0900

    css수정, auditing적용

--- src/main/java/com/softj/pwg/PwgApplication.java
+++ src/main/java/com/softj/pwg/PwgApplication.java
@@ -2,7 +2,9 @@ package com.softj.pwg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PwgApplication{
 
